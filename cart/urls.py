b8b7285fa877414f79ca998d875ac3f3e28133e5
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-07-28 23:20:37 +0300

    Django4.0.6+Python3.9 App

---
+++ cart/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.urls import handler400, handler500, include, patterns, url
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    url(r'^$', views.cart_detail, name='cart_detail'),
+    url(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
+    url(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart_remove'),
+]
